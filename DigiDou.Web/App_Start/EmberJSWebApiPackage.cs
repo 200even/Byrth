55564d794459bf1e993753c939061b3baddd38de
Scott Ferguson <esfergus@gmail.com>
2015-09-03 15:28:57 -0500

    Inlined the emberjs webapi library

--- DigiDou.Web/App_Start/EmberJSWebApiPackage.cs
+++ DigiDou.Web/App_Start/EmberJSWebApiPackage.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Formatting;
 using System.Web.Http;
-using EmberJS.WebAPI;
 
 [assembly: WebActivatorEx.PostApplicationStartMethod(typeof(DigiDou.Web.App_Start.EmberJSWebApiPackage), "PostStart")]
 namespace DigiDou.Web.App_Start {
