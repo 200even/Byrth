36dc028b63fa6e8854459a9e31f13f6cdbe5056f
Scott Ferguson <esfergus@gmail.com>
2015-09-07 13:15:28 -0500

    Added contractions MVC. Edits to Contraction class

---
+++ DigiDou.Web/Controllers/ContractionsMvcController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DigiDou.Web.Models;
+
+namespace DigiDou.Web.Controllers
+{
+    public class ContractionsMvcController : BaseController
+    {
+        // GET: ContractionsMvc
+        public ActionResult Index()
+        {
+            return View(CurrentUser.Contractions.ToList());
+        }
+
+        // GET: ContractionsMvc/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contraction contraction = CurrentUser.Contractions.Find(c => c.Id == id);
+            if (contraction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contraction);
+        }
+
+        // GET: ContractionsMvc/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContractionsMvc/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,StartTime,EndTime")] Contraction contraction)
+        {
+            if (ModelState.IsValid)
+            {
+                CurrentUser.Contractions.Add(contraction);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contraction);
+        }
+
+        // GET: ContractionsMvc/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null || CurrentUser.Contractions.Any(c => c.Id != id))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contraction contraction = db.Contractions.Find(id);
+            if (contraction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contraction);
+        }
+
+        // POST: ContractionsMvc/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,StartTime,EndTime")] Contraction contraction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contraction).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contraction);
+        }
+
+        // GET: ContractionsMvc/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contraction contraction = CurrentUser.Contractions.Find(c => c.Id == id);
+            if (contraction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contraction);
+        }
+
+        // POST: ContractionsMvc/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contraction contraction = db.Contractions.Find(id);
+            CurrentUser.Contractions.Remove(contraction);
+            db.Contractions.Remove(contraction);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- DigiDou.Web/Models/Contraction.cs
+++ DigiDou.Web/Models/Contraction.cs
@@ -13,7 +13,24 @@ namespace DigiDou.Web.Models
         public DateTime EndTime { get; set; }
 
         public TimeSpan Length => EndTime - StartTime;
-
+        public TimeSpan TimeSinceLast
+        {
+            get
+            {
+                ApplicationDbContext db = new ApplicationDbContext();
+                //CurrentUser= db.Users.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
+                ApplicationUser CurrentUser = db.Users.FirstOrDefault();
+                if (CurrentUser.Contractions.ElementAt(Id) == CurrentUser.Contractions.ElementAt(0))
+                {
+                    return TimeSpan.Zero;
+                }
+                else
+                {
+                    //var calculate = CurrentUser.Contractions.Where()//.EndTime - StartTime
+                    return TimeSpan.Zero;
+                }
+            }
+        }
         public virtual ApplicationUser User { get; set; }
 
     }
